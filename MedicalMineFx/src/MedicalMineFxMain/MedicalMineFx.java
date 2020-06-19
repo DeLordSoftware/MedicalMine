9851939f9e74344dc0efe5ae071ef44418b3f401
DeLordSoftware <holynupe489@hotmail.com>
2020-06-19 02:33:27 -0400

    Created Encoder project - ver 1

--- MedicalMineFx/src/MedicalMineFxMain/MedicalMineFx.java
+++ MedicalMineFx/src/MedicalMineFxMain/MedicalMineFx.java
@@ -1,8 +1,14 @@
-
 package MedicalMineFxMain;
 
 import static MedicalMineFxMain.MedicalMineFx.MainResource;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
@@ -16,77 +22,169 @@ import javafx.stage.Stage;
  *
  * @author RW Simmons
  */
-public class MedicalMineFx extends Application {    
-    
-    private static final String VERSION = "4.0";
-    private static final String TITLE_NAME = "Seek-N-Shall Fine by DSC ver ";     
-    public static String MainScreen      = "main";
-    public static String MainResource    = "/FxmlDisplays/SelectInputData.fxml";
-    public static String ProgessScreen   = "progess";
+public class MedicalMineFx extends Application {
+
+    private static final String VERSION = "1.4.0";
+    private static final String TITLE_NAME = "Seek-N-Shall Find by DSC ver ";
+    public static String MainScreen = "main";
+    public static String MainResource = "/FxmlDisplays/SelectInputData.fxml";
+    public static String ProgessScreen = "progess";
     public static String ProgessResource = "/FxmlDisplays/ProcessFileDisplay.fxml";
-    public static String LoadScreen      = "load";
-    
+    public static String LoadScreen = "load";
+
     private static Stage mainStage;
     private static Stage progessStage;
     private static Stage currentStage;
-    
-    public static Stage getStage(){
+
+    public static Stage getStage() {
         return currentStage;
-    }    
-    
+    }
+
     @Override
-    public void start(Stage stage) throws Exception {        
-        try{
-            CreateStage createStage = new CreateStage();           
+    public void start(Stage stage) throws Exception {
+        try {
+            CreateStage createStage = new CreateStage();
             currentStage = stage;
-            mainStage = createStage.setStage(stage, MainResource, TITLE_NAME + VERSION);   
+            mainStage = createStage.setStage(stage, MainResource, TITLE_NAME + VERSION);
             mainStage.getIcons().add(new Image("/Images/Seek-N-Shall Find Image.png"));
             mainStage.show();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("ERROR 2: " + e.getMessage());
-        }       
-            
+        }
+
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws IOException {
+        if (checkLicense()) {
+            launch(args);
+        } else {
+            // Display error message and close application 
+            System.out.println("Expired license!!!");
+        }
     }
-    
+
     /**
-     * 
+     * showProgessPage
      */
-    public static void showProgessPage(){
+    public static void showProgessPage() {
         CreateStage createStage = new CreateStage();
-        progessStage = createStage.setStage(currentStage, ProgessResource, "");          
+        progessStage = createStage.setStage(currentStage, ProgessResource, "");
     }
-    
+
+    private static boolean checkLicense() {
+        boolean returnVal = false;
+        try {
+            File file = new File("License.lic");
+            Scanner sc = new Scanner(file);
+            // Get actual expiration date
+            String strExpire = sc.nextLine();
+            System.out.println(strExpire);
+            // Get actual expiration encoder
+            String strEncoder = sc.nextLine();
+            System.out.println(strEncoder);
+
+            char[] lstEncoder = strEncoder.toCharArray();
+            int[] lstLocate = new int[3];
+            int iMonth = 0;
+            int iDay = 0;
+            int iYear = 0;
+
+            int count = 0;
+            int iValue = 0;
+
+            // Convert string to integer list
+            for (int ii = 0; ii < 3; ii++) {
+                if (lstEncoder[ii] == 'A') {
+                    // Convert letter to integer 
+                    iValue = 10;
+                } else {
+                    iValue = Integer.parseInt(String.valueOf(lstEncoder[ii]));
+                }
+                switch (ii) {
+                    case 0:
+                        // Month
+                        iMonth = iValue;
+                        break;
+                    case 1:
+                        // Day
+                        iDay = iValue;
+                        break;
+                    case 2:
+                        // Year
+                        iYear = iValue;
+                        break;
+                }
+            }
+
+            // Collect expiration date with in encoded data
+            String strMonthLic = String.valueOf(lstEncoder[iMonth]);
+            strMonthLic = strMonthLic.concat(String.valueOf(lstEncoder[iMonth + 1]));
+            int iMonthLic = Integer.parseInt(strMonthLic);
+
+            String strDayLic = String.valueOf(lstEncoder[iDay]);
+            strDayLic = strDayLic.concat(String.valueOf(lstEncoder[iDay + 1]));
+            int iDayLic = Integer.parseInt(strDayLic);
+
+            String strYearLic = String.valueOf(lstEncoder[iYear]);
+            strYearLic = strYearLic.concat(String.valueOf(lstEncoder[iYear + 1]));
+            int iYearLic = Integer.parseInt(strYearLic);
+
+            // Get current date
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yy");
+            Date date = new Date();
+            String strCurrentDate = formatter.format(date);
+            String[] lstDate = strCurrentDate.split("/");
+
+            int iCurrentMonth = Integer.parseInt(lstDate[0]);
+            int iCurrentDay = Integer.parseInt(lstDate[1]);
+            int iCurrentYear = Integer.parseInt(lstDate[2]);
+
+            // Check to see if license has expried
+            if (iYearLic > iCurrentYear) {
+                returnVal = true;
+            } else if (iYearLic == iCurrentYear) {
+                if (iMonthLic >= iCurrentMonth) {
+                    returnVal = true;
+                } else {
+                    returnVal = false;
+                }
+            } else {
+                returnVal = false;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(MedicalMineFx.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return returnVal;
+    }
+
     /**
-     * 
-     * @throws InterruptedException 
+     *
+     * @throws InterruptedException
      */
-    public static void closeProgessPage() throws InterruptedException{
-       CreateStage createStage = new CreateStage();
-       mainStage = createStage.setStage(currentStage, MainResource, TITLE_NAME  + VERSION);
-       mainStage.show();
+    public static void closeProgessPage() throws InterruptedException {
+        CreateStage createStage = new CreateStage();
+        mainStage = createStage.setStage(currentStage, MainResource, TITLE_NAME + VERSION);
+        mainStage.show();
     }
 }
 
-/*****************************************
-* Class:
-* Purpose:  
-****************************************/
-class CreateStage{
-    
-    public Stage setStage(Stage stage, String strResource, String title){
+/**
+ * ***************************************
+ * Class: Purpose: **************************************
+ */
+class CreateStage {
+
+    public Stage setStage(Stage stage, String strResource, String title) {
         try {
             FXMLLoader myLoader = new FXMLLoader(getClass().getResource(strResource));
-            Parent loadScreen =(Parent) myLoader.load();
-            Scene scene =  new Scene(loadScreen);
+            Parent loadScreen = (Parent) myLoader.load();
+            Scene scene = new Scene(loadScreen);
             stage.setScene(scene);
-            stage.setTitle(title); 
+            stage.setTitle(title);
         } catch (IOException ex) {
             Logger.getLogger(CreateStage.class.getName()).log(Level.SEVERE, null, ex);
         }
---
+++ SeekLicenseEncoder/src/fxmllicenseencoder/FXMLLicenseEncoderController.java
@@ -0,0 +1,416 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxmllicenseencoder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author RW Simmons
+ */
+public class FXMLLicenseEncoderController implements Initializable {
+
+    final String YEAR_LIC = "1 Year";
+    final String MONTH_9_LIC = "9 Months";
+    final String MONTH_6_LIC = "6 Months";
+    final String MONTH_3_LIC = "3 Months";
+    final String MONTH_1_LIC = "1 Months";
+    final String CUSTOM_LIC = "Custom";
+    final int MONTH_VAL = 0;
+    final int DAY_VAL = 1;
+    final int YEAR_VAL = 2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Set combo box
+        cmbSelectLength.getItems().add("");
+        cmbSelectLength.getItems().add(YEAR_LIC);
+        cmbSelectLength.getItems().add(MONTH_9_LIC);
+        cmbSelectLength.getItems().add(MONTH_6_LIC);
+        cmbSelectLength.getItems().add(MONTH_3_LIC);
+        cmbSelectLength.getItems().add(MONTH_1_LIC);
+        cmbSelectLength.getItems().add(CUSTOM_LIC);
+
+        // Get current date
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yy");
+        Date date = new Date();
+        txtSystDate.setText(formatter.format(date));
+    }
+
+    /**
+     *
+     * @param selected
+     * @param date
+     */
+    private void createEncoderString(String selected, String date) throws IOException {
+        // create instance of Random class 
+        int[] lstDataLocation = setDateLocations();
+        int iMonth = lstDataLocation[MONTH_VAL];
+        int iDay = lstDataLocation[DAY_VAL];
+        int iYear = lstDataLocation[YEAR_VAL];
+        System.out.println("Month " + iMonth + " Day " + iDay + " Year " + iYear);
+
+        // Prefill encoder list
+        String[] lstDate = date.split("/");
+        int[] lstEncoder = new int[12];
+        Random random = new Random();
+
+        for (int ii = 2; ii < lstEncoder.length; ii++) {
+            lstEncoder[ii] = random.nextInt(9);
+        }
+
+        lstEncoder[0] = iMonth;
+        lstEncoder[1] = iDay;
+        lstEncoder[2] = iYear;
+        presetEncoderList(lstDate, lstEncoder, lstDataLocation);
+        int iChangeDay0 = lstEncoder[iMonth];
+        int iChangeDay1 = lstEncoder[iMonth + 1];
+
+        // Create encoder data
+        switch (selected) {
+            case YEAR_LIC:
+                // One Year
+                setYear(lstEncoder, iYear);
+                break;
+            case MONTH_9_LIC:
+                // Nine Month
+                if (iChangeDay0 == 0 && iChangeDay1 == 1) {
+                    // One - 10
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 0;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 2) {
+                    // Two - 11
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 1;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 3) {
+                    // Three - 12
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 2;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 4) {
+                    // Four - 01
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 1;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 5) {
+                    // Five - 02
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 2;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 6) {
+                    // Six - 03
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 3;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 7) {
+                    // Seven - 04    
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 4;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 8) {
+                    // Eight - 05
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 5;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 9) {
+                    // Nine - 06
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 6;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 0) {
+                    // Ten - 07
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 7;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 1) {
+                    // Eleven - 08   
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 8;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 2) {
+                    // Twelve - 09
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 9;
+                    setYear(lstEncoder, iYear);
+                } else {
+                    lstEncoder[iMonth + 1] += 9;
+                }
+                break;
+            case MONTH_6_LIC:
+                // Six Month
+                if (iChangeDay0 == 0 && iChangeDay1 == 4) {
+                    // Four - 10 
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 0;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 5) {
+                    // Five - 11
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 1;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 6) {
+                    // Six - 12
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 2;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 7) {
+                    // Seven - 01    
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 1;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 8) {
+                    // Eight - 02
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 2;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 9) {
+                    // Nine - 03
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 3;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 0) {
+                    // Ten - 04 
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 4;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 1) {
+                    // Eleven - 05   
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 5;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 2) {
+                    // Twelve - 06
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 6;
+                    setYear(lstEncoder, iYear);
+                } else {
+                    lstEncoder[iMonth + 1] += 6;
+                }
+                break;
+            case MONTH_3_LIC:
+                // Three Month
+                if (iChangeDay0 == 0 && iChangeDay1 == 7) {
+                    // Seven - 10    
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 0;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 8) {
+                    // Eight - 11
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 1;
+                } else if (iChangeDay0 == 0 && iChangeDay1 == 9) {
+                    // Nine - 12
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 2;
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 0) {
+                    // Ten - 01
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 1;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 1) {
+                    // Eleven - 02   
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 2;
+                    setYear(lstEncoder, iYear);
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 2) {
+                    // Twelve - 03
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 3;
+                    setYear(lstEncoder, iYear);
+                } else {
+                    lstEncoder[iMonth + 1] += 3;
+                }
+                break;
+            case MONTH_1_LIC:
+                // One Month
+                if (iChangeDay0 == 0 && iChangeDay1 == 9) {
+                    // Nine
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 0;
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 0) {
+                    // Ten
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 1;
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 1) {
+                    // Eleven    
+                    lstEncoder[iMonth] = 1;
+                    lstEncoder[iMonth + 1] = 2;
+                } else if (iChangeDay0 == 1 && iChangeDay1 == 2) {
+                    // Twelve 
+                    lstEncoder[iMonth] = 0;
+                    lstEncoder[iMonth + 1] = 1;
+                    setYear(lstEncoder, iYear);
+                } else {
+                    lstEncoder[iMonth + 1] += 1;
+                }
+                break;
+            case CUSTOM_LIC:
+                // TODO
+                break;
+        }
+
+        // Create Encoder license 
+        String strFinalEncoder = createEncoderFile(lstEncoder);
+
+        // Display results
+        txtEncoderResult.setText("Encoder # - " + strFinalEncoder);
+        String strExpire = lstEncoder[iMonth] + lstEncoder[iMonth + 1] + "/" + lstEncoder[iDay] + lstEncoder[iDay + 1] + "/" + lstEncoder[iYear] + lstEncoder[iYear + 1];
+        txtExpire.setText("Expires: " + strExpire);
+
+        // Print to file
+        FileWriter flEncoderLic = null;
+        try {
+            flEncoderLic = new FileWriter("License.lic");
+            flEncoderLic.write(strExpire + "\n" + strFinalEncoder);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLLicenseEncoderController.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            flEncoderLic.close();
+        }
+    }
+
+    /**
+     *
+     * @param iMonth
+     * @param iDay
+     * @param iYear
+     * @return
+     */
+    private int[] setDateLocations() {
+        int iMonth;
+        int iDay;
+        int iYear;
+        Random random = new Random();
+
+        // Get month
+        do {
+            iMonth = random.nextInt(11);
+        } while (iMonth < 3);
+
+        // Get Day
+        boolean bHasMonth = true;
+        do {
+            iDay = random.nextInt(11);;
+            if (iDay > 3) {
+                if ((iMonth != iDay) && (iMonth + 1 != iDay) && (iMonth - 1 != iDay)) {
+                    bHasMonth = false;
+                }
+            }
+        } while (bHasMonth);
+
+        // Get year
+        boolean bHasYear = true;
+        do {
+            iYear = random.nextInt(11);
+            if (iYear > 3) {
+                if ((iYear != iDay) && (iYear + 1 != iDay) && (iYear - 1 != iDay)) {
+                    if ((iYear != iMonth) && (iYear + 1 != iMonth) && (iYear - 1 != iMonth)) {
+                        bHasYear = false;
+                    }
+                }
+            }
+        } while (bHasYear);
+
+        int[] returnList = {iMonth, iDay, iYear};
+        return returnList;
+    }
+
+    /**
+     *
+     * @param lstDate
+     * @param lstEncode
+     * @param date
+     */
+    private void presetEncoderList(String[] lstDate, int[] lstEncode, int[] date) {
+        int iMonth = date[MONTH_VAL];
+        int iDay = date[DAY_VAL];
+        int iYear = date[YEAR_VAL];
+
+        char[] strList = lstDate[MONTH_VAL].toCharArray();
+        lstEncode[iMonth] = Integer.parseInt(Character.toString(strList[0]));
+        lstEncode[iMonth + 1] = Integer.parseInt(Character.toString(strList[1]));
+
+        strList = lstDate[DAY_VAL].toCharArray();
+        lstEncode[iDay] = Integer.parseInt(Character.toString(strList[0]));
+        lstEncode[iDay + 1] = Integer.parseInt(Character.toString(strList[1]));
+
+        strList = lstDate[YEAR_VAL].toCharArray();
+        lstEncode[iYear] = Integer.parseInt(Character.toString(strList[0]));
+        lstEncode[iYear + 1] = Integer.parseInt(Character.toString(strList[1]));
+    }
+
+    /**
+     *
+     * @param lst
+     * @return
+     */
+    private String createEncoderFile(int[] lst) {
+        String strFinalEncoder = new String();
+        int count = 0;
+        for (int ii : lst) {
+            if (ii > 9) {
+                switch (ii) {
+                    case 10:
+                        strFinalEncoder = strFinalEncoder.concat("A");
+                        break;
+                    case 11:
+                        strFinalEncoder = strFinalEncoder.concat("B");
+                        break;
+                }
+            } else {
+                //System.out.println(count++ + " Encoder - " + ii);
+                strFinalEncoder = strFinalEncoder.concat(String.valueOf(ii));
+            }
+        }
+
+        return strFinalEncoder;
+    }
+
+    /**
+     *
+     * @param list
+     * @param year
+     */
+    private void setYear(int[] list, int year) {
+        if (list[year + 1] == 9) {
+            list[year] += 1;
+            list[year + 1] = 0;
+        } else {
+            list[year + 1] += 1;
+        }
+    }
+
+    @FXML
+    private ComboBox<String> cmbSelectLength;
+    @FXML
+    private Label txtSystDate;
+    @FXML
+    private Button btnCreateLic;
+    @FXML
+    private Label txtEncoderResult;
+    @FXML
+    private Label txtExpire;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        String strSelectedItem = cmbSelectLength.getSelectionModel().getSelectedItem();
+        String strDate = txtSystDate.getText();
+        createEncoderString(strSelectedItem, strDate);
+    }
+
+    @FXML
+    private void handleComboBox(ActionEvent event) {
+    }
+}
